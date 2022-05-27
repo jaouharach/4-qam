7353fca734e7df026a19a850e4c8f7ec7997c0c7
Jaouhara Chanchaf <52602313+jaouharach@users.noreply.github.com>
2022-05-27 13:09:57 +0100

    average BER

--- 4-qam.py
+++ 4-qam.py
@@ -26,45 +26,52 @@ def approximate(data_point, modulation):
             closest_mod = m
     return closest_mod
 
+# original vector
 data = [ random.choice([0, 1]) for i in range(360)]
-print(data)
+# print(data)
 
-# generate the noise 
-mu, sigma = 0, math.sqrt(1/10 ** (3/10)) # mean and standard deviation
-nr = np.random.normal(mu, sigma,  int(len(data)/2))
-ni = np.random.normal(mu, sigma,  int(len(data)/2))
-noise = [complex(nr[i],ni[i]) for i in range(int(len(data)/2))]
+avg_ber = 0
+num_iter = 1000
+for k in range(num_iter):
+    # generate the noise 
+    mu, sigma = 0, math.sqrt(1/10 ** (3/10)) # mean and standard deviation
+    nr = np.random.normal(mu, sigma,  int(len(data)/2))
+    ni = np.random.normal(mu, sigma,  int(len(data)/2))
+    noise = [complex(nr[i],ni[i]) for i in range(int(len(data)/2))]
 
-# print("\n\n\nNoise:\n\n\n")
-# print(noise)
+    # print("\n\n\nNoise:\n\n\n")
+    # print(noise)
 
-i = 0
-j = 0
-recieved_signal = []
+    i = 0
+    j = 0
+    recieved_signal = []
 
-while(i < len(data)):
-    data_point = str(data[i]) + str (data[i+1])
-    recieved_signal.append(qam.get(data_point)+noise[j])
-    i = i + 2
-    j = j + 1
+    while(i < len(data)):
+        data_point = str(data[i]) + str (data[i+1])
+        recieved_signal.append(qam.get(data_point)+noise[j])
+        i = i + 2
+        j = j + 1
 
-# print(f"\n\n\nRecieved signal (length = {len(recieved_signal)}):\n\n\n")
-# print(recieved_signal)
+    # print(f"\n\n\nRecieved signal (length = {len(recieved_signal)}):\n\n\n")
+    # print(recieved_signal)
 
-# approximate the original signal
-approx_signal = []
-for s in recieved_signal:
-    bits = approximate(s, qam)
-    approx_signal.append(int(bits[0]))
-    approx_signal.append(int(bits[1]))
+    # approximate the original signal
+    approx_signal = []
+    for s in recieved_signal:
+        bits = approximate(s, qam)
+        approx_signal.append(int(bits[0]))
+        approx_signal.append(int(bits[1]))
 
-print(approx_signal)
+    # print(approx_signal)
 
 
-# compute the BER
-ber = 0
-for i in range(int(len(data))):
-    if approx_signal[i] != data[i]:
-        ber = ber + 1
+    # compute the BER
+    ber = 0
+    for i in range(int(len(data))):
+        if approx_signal[i] != data[i]:
+            ber = ber + 1
 
-print(f"Bit error rate = {ber/len(data)}")
+    ber = ber/len(data)
+    avg_ber = avg_ber + ber
+
+print(f"Average bit error rate = {avg_ber/num_iter}")
